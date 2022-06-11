9910c3624491d97cdd18054489316e8a35f2ec49
sellamiselwa <107304437+sellamiselwa@users.noreply.github.com>
2022-06-12 00:56:51 +0200

    #8 ffff
    #8 ffff

--- Project-2/Program.cs
+++ Project-2/Program.cs
@@ -6,7 +6,7 @@ namespace Project_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! qqqq");
         }
     }
 }
